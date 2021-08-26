7aa9e3e1a766f4c002c17e5c00c12a9604e00052
zerovip <lizihevip@163.com>
2021-08-26 17:31:05 +0800

    the table of record margin in centre, completely white before all loaded when using dark mode.

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,12 +17,6 @@ function ChangeClassBlackBlock(wait_to_change) {
 }
 
 // 切换颜色主题
-// 定义函数，功能是：获取以 cname 为键的 cookie 值
-// 这是 jQuary.cookie 中用的方法，见，https://stackoverflow.com/q/5639346/9783145
-function read_cookie(key) {
-    var result;
-    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? (result[1]) : null;
-}
 // utterances 的改变主题函数，见 https://github.com/utterance/utterances/issues/170
 function ut_change_to_light_mode() {
     try {
@@ -46,44 +40,60 @@ function ut_change_to_dark_mode() {
     }
     catch(err) {}
 }
-// 初始化
-const checkbox = document.querySelector(".theme-switcher");
-const themeContainer = document.querySelector(".theme-container");
-// 获取 cookie
-var ctheme = read_cookie("ctheme");
-// 如果 cookie 里有值就按 cookie 的方法去做
-// 如果 cookie 里没有值就看 checkbox 的默认值
-// checkbox 的默认值看 single.html
-if (ctheme == "dark") {
-    themeContainer.classList.add("dark");
-    checkbox.checked = false;
-    ut_change_to_dark_mode();
-} else if (ctheme == "light") {
-    themeContainer.classList.remove("dark");
-    checkbox.checked = true;
-    ut_change_to_light_mode();
-} else {
+
+// 主题变化监听函数，在 checkbox 状态改变时改写 css 变量并改写 localStorage，
+// 同时对 utterance （有文章页有）的主题做出改变
+function themeSwitch() {
     if (checkbox.checked) {
         themeContainer.classList.remove("dark");
-        document.cookie = "ctheme=light; path=/; SameSite=Lax";
+        localStorage.setItem("ctheme", "light");
         ut_change_to_light_mode();
     } else {
         themeContainer.classList.add("dark");
-        document.cookie = "ctheme=dark; path=/; SameSite=Lax";
+        localStorage.setItem("ctheme", "dark");
         ut_change_to_dark_mode();
     }
 }
-// 添加监听函数，在 checkbox 状态改变时改写 css 变量并改写 cookie，
-// 同时对 utterance （有文章页有）的主题做出改变
-checkbox.addEventListener("change", function() {
-    if (themeContainer && this.checked) {
+
+// 下面是修正换页面时的闪烁，参见:
+// https://zwbetz.com/fix-the-white-flash-on-page-load-when-using-a-dark-theme-on-a-static-site/
+function showTheme() {
+    // 获取 localStorage
+    var ctheme = localStorage.getItem('ctheme');
+    // 如果 localStorage 里有值就按 localStorage 的方法去做
+    // 如果 localStorage 里没有值就看 checkbox 的默认值
+    // checkbox 的默认值看 baseof.html
+    if (ctheme == "dark") {
+        themeContainer.classList.add("dark");
+        checkbox.checked = false;
+        ut_change_to_dark_mode();
+    } else if (ctheme == "light") {
         themeContainer.classList.remove("dark");
-        document.cookie = "ctheme=light; path=/; SameSite=Lax";
+        checkbox.checked = true;
         ut_change_to_light_mode();
     } else {
-        themeContainer.classList.add("dark");
-        document.cookie = "ctheme=dark; path=/; SameSite=Lax";
-        ut_change_to_dark_mode();
+        if (checkbox.checked) {
+            themeContainer.classList.remove("dark");
+            localStorage.setItem("ctheme", "light");
+            ut_change_to_light_mode();
+        } else {
+            themeContainer.classList.add("dark");
+            localStorage.setItem("ctheme", "dark");
+            ut_change_to_dark_mode();
+        }
     }
+}
+
+function showContent() {
+    document.body.style.visibility = 'visible';
+    document.body.style.opacity = 1;
+}
+
+// 初始化
+const checkbox = document.querySelector(".theme-switcher");
+const themeContainer = document.querySelector(".theme-container");
+window.addEventListener('DOMContentLoaded', function () {
+    showTheme();
+    showContent();
 });
 

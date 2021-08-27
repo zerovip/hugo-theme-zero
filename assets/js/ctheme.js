eeb9f64197bd28490bb57cce5af2197798efd55a
zerovip <lizihevip@163.com>
2021-08-27 18:55:38 +0800

    prefers-color-scheme support, image lazy load support.

--- assets/js/ctheme.js
+++ assets/js/ctheme.js
@@ -1,6 +1,6 @@
 // 初始化
 const themeContainer = document.querySelector(".theme-container");
-var ctheme = localStorage.getItem('ctheme');
+var ctheme = localStorage.getItem("ctheme");
 var checked = 1;
 // 如果 localStorage 里有值就按 localStorage 的方法去做
 // 如果 localStorage 里没有值就看用户系统的偏好
@@ -9,4 +9,12 @@ if (ctheme == "dark") {
     checked = 0;
 } else if (ctheme == "light") {
     themeContainer.classList.remove("dark");
+} else {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        themeContainer.classList.add("dark");
+        checked = 0;
+    } else {
+        themeContainer.classList.remove("dark");
+    }
 }
+
